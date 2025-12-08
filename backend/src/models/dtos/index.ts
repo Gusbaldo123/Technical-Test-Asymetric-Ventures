676a635fd70ae76041550d8d1786a667c1bf38e9
Gustavo Rogério Bernardino Pereira <42393049+Gusbaldo123@users.noreply.github.com>
2025-12-08 19:04:10 -0300

    DTO reorganization
    Fixed:
     - Simplified DTOs

--- backend/src/models/dtos/index.ts
+++ backend/src/models/dtos/index.ts
@@ -1,5 +1,5 @@
 // models/dtos/AuthorDTO/index.ts
-export * from './AuthorDTO/RegisterAuthorDTO';
-export * from './AuthorDTO/UpdateAuthorDTO';
-export * from './AuthorDTO/AuthorResponseDTO';
-export * from './AuthorDTO/LoginAuthorDTO';
+export * from './AuthorDTO/RegisterDTO';
+export * from './AuthorDTO/UpdateDTO';
+export * from './AuthorDTO/ProfileDTO';
+export * from './AuthorDTO/LoginDTO';
