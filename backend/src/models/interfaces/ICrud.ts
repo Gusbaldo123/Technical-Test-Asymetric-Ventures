3e9dc9ff4a49b817a62ba35c37a9f639b4b9501b
Gustavo Rogério Bernardino Pereira <42393049+Gusbaldo123@users.noreply.github.com>
2025-12-08 17:42:02 -0300

    Missed boolean on interface

--- backend/src/models/interfaces/ICrud.ts
+++ backend/src/models/interfaces/ICrud.ts
@@ -2,7 +2,7 @@ interface ICrud<T,TDTO> {
     create(model: T): Promise<TDTO | null>;
     getById(id: number): Promise<TDTO | null>;
     updateById(id: number, model: T): Promise<TDTO|null>;
-    deleteById(id: number): Promise<void>;
+    deleteById(id: number): Promise<boolean>;
 }
 
 export { ICrud }
