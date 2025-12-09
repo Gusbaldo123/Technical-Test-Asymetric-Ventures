9f861752e969faf9f45b034f86da1c1b004ffd8e
Gustavo Rogério Bernardino Pereira <42393049+Gusbaldo123@users.noreply.github.com>
2025-12-09 19:55:36 -0300

    AI post generation
    Added:
     - AI agent to generate posts using JSON

--- backend/src/managers/JobManager.ts
+++ backend/src/managers/JobManager.ts
@@ -7,6 +7,10 @@ import { AuthorService } from '../services/AuthorService';
 import { PostCreateDTO } from '../models/dtos';
 import { AuthorResponseDTO } from '../models/dtos';
 
+import generatePost from '../models/agent/agent';
+
+
+
 class JobManager {
     private static instance: JobManager;
     private postService!: PostService;
@@ -39,22 +43,27 @@ class JobManager {
             max = Math.floor(max); // Ensure max is an integer
             return Math.floor(Math.random() * (max - min + 1)) + min;
         }
-        console.log("Creating new post...");
+        console.log("\n\nCreating new post...");
         const randomCategories = await this.categoryService.getRandomCategories(getRandomInt(1, 3));
         if (!randomCategories) {
             console.log(`Unreachable category found.`);
             return null;
         }
 
-        const post: PostCreateDTO = { title: "Título", content: "MUITO TEXTO, MUITO MUITO TEXTO", categoryIds: randomCategories.map(cat => cat.id) };
-        const author: AuthorResponseDTO|null = await this.authorService.getById(1);
+        const post: PostCreateDTO|null = await generatePost(randomCategories);
+        if (!post) {
+            console.log(`Post unavaliable`);
+            return null;
+        }
+
+        const author: AuthorResponseDTO | null = await this.authorService.getById(1);
         if (!author) {
             console.log(`Unreachable author found.`);
             return null;
         }
 
         const createdPost = await this.postService.create(post, author);
-        console.log(createdPost);
+        console.log(`Title: ${createdPost?.title}\nContent: ${createdPost?.content}\nCategories: ${randomCategories.map(cat=>cat.name)}`);
     }
 
     public startAll(): void {
